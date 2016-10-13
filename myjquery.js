d3285c90c53758ce08f134f06e17a9b9c23fe72b
Patrick Segura <psegur@Patricks-MacBook-Pro-2.local>
2016-10-13 15:59:45 -0500

    Phase2

--- myjquery.js
+++ myjquery.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 	$('#aboutme').mouseenter(function () {
         $('.header').toggleClass('header2', 1000);
+        $('#myname').css('letter-spacing' , '50px');
 	});
 	$('#aboutme').mouseleave(function () {
         $('.header').toggleClass('header2', 1000);
+        $('#myname').css('letter-spacing' , '20px');
 	});
 	$('#menu-toggle').mouseenter(function () {
+		$('.header').css('opacity', '1');
+	});
+	$('.header').mouseleave(function () {
+		$('.header').css('opacity', '0');
+	});
+	$('.header').mouseenter(function () {
 		$('.header').css('opacity', '1');
 	});
 });
